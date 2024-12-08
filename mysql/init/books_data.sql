ba447fbffde4d2259ba7649c84e6960a90c5f4b6
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2024-12-08 19:20:51 +0900

    初期データ4件登録

--- mysql/init/books_data.sql
+++ mysql/init/books_data.sql
@@ -22,4 +22,6 @@ CREATE TABLE books (
 INSERT INTO books (isbn, title, author, genre, price, image_url, detail)
 VALUES
   ('9784297124373', 'プロを目指す人のためのRuby入門[改訂2版]', '伊藤 淳一', 'Ruby', 3278, 'https://i.gyazo.com/9cbb0ba3c2040dc3dc28ae5317a14a10.jpg', '通称チェリー本です。'),
-  ('9784798144450', 'SQLゼロからはじめるデータベース操作', 'ミック', 'SQL', 2068, 'https://m.media-amazon.com/images/I/71wBt9D4wIL._SY522_.jpg', 'postgreSQLで実際にDB操作ができる' );
+  ('9784798144450', 'SQLゼロからはじめるデータベース操作', 'ミック', 'SQL', 2068, 'https://m.media-amazon.com/images/I/71wBt9D4wIL._SY522_.jpg', 'postgreSQLで実際にDB操作ができる' ),
+  ('9784873115658', 'リーダブルコード', 'Dustin Boswell', '技術書', 2640, 'https://m.media-amazon.com/images/I/81+3DpjuMdL._SY522_.jpg', 'より良いコードを書くためのテクニック' ),
+  ('9784802614061', 'システム設計の面接試験', 'アレックス・シュウ', '技術書', 3080, 'https://m.media-amazon.com/images/I/81JWwksE-9L._SY522_.jpg', '中規模~大規模システムってどんな感じで動くか' );
