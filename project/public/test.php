ee5980a2f8a3bfc148c28591f69d48ed5eacf95e
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2025-01-06 07:09:07 +0900

    配列

--- project/public/test.php
+++ project/public/test.php
@@ -5,23 +5,15 @@ $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
 
 try {
   $pdo = new PDO($dsn, $user, $password);
-  $sql = "SELECT * FROM books WHERE id = :id";
+  $sql = "SELECT * FROM books";
   $stmt = $pdo->prepare($sql);
-  $stmt->execute(['id' => 3]);
-  $reading_lists = $stmt->fetch(PDO::FETCH_ASSOC);
+  $stmt->execute();
+  $reading_lists = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
 
-// if ($reading_lists) {
-//   echo json_encode($reading_lists); // JSON形式で表示
-// } else {
-//   echo "データが見つかりませんでした。"; // データがない場合のメッセージ
-// }
-// echo $reading_lists;
-// echo ($dsn);
-// echo ($reading_list);
 ?>
 
 <a href="index.php">TOPページ</a>
@@ -40,13 +32,13 @@ try {
             <th>詳細</th>
         </tr>
         <tr>
-            <td><?php echo htmlspecialchars($reading_lists['isbn'], ENT_QUOTES, 'UTF-8'); ?></td>
-            <td><?php echo htmlspecialchars($reading_lists['title'], ENT_QUOTES, 'UTF-8'); ?></td>
-            <td><?php echo htmlspecialchars($reading_lists['author'], ENT_QUOTES, 'UTF-8'); ?></td>
-            <td><?php echo htmlspecialchars($reading_lists['genre'], ENT_QUOTES, 'UTF-8'); ?></td>
-            <td><?php echo htmlspecialchars($reading_lists['price'], ENT_QUOTES, 'UTF-8'); ?></td>
-            <td><img src="<?php echo htmlspecialchars($reading_lists['image_url'], ENT_QUOTES, 'UTF-8'); ?>" alt="book image"></td>
-            <td><?php echo htmlspecialchars($reading_lists['detail'], ENT_QUOTES, 'UTF-8'); ?></td>
+        <td><?php echo htmlspecialchars($reading_lists[0]['isbn'], ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($reading_lists[0]['title'], ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($reading_lists[0]['author'], ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($reading_lists[0]['genre'], ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars($reading_lists[0]['price'], ENT_QUOTES, 'UTF-8'); ?></td>
+            <td><img src="<?php echo htmlspecialchars($reading_lists[0]['image_url'], ENT_QUOTES, 'UTF-8'); ?>" alt="book image"></td>
+            <td><?php echo htmlspecialchars($reading_lists[0]['detail'], ENT_QUOTES, 'UTF-8'); ?></td>
         </tr>
     </table>
 <?php else: ?>
