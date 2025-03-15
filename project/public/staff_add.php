526ab097bf1a6a8a78eb2644d837d0adef6b508c
石井悠香 <i.yu-ka@ishiiyukounoAir.lan>
2025-03-15 19:23:25 +0900

    調査

---
+++ project/public/staff_add.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>スタッフ登録フォーム</title>
+</head>
+<body>
+  (登録フォーム作成中)
+  スタッフ登録<br />
+  <br />
+  <form method="post" action="staff_add_check.php">
+  スタッフ名を入力<br />
+  <input type="text" name="name" style="width:100px"><br />
+  パスワードを入力<br />
+  <input type="password" name="pass" style="width:100px"><br />
+  パスワードをもう一度入力<br />
+  <input type="password" name="pass2" style="width:100px"><br />
+  <br />
+  <input type="button" onclick="history.back()" value="戻る">
+  <input type="submit" value="OK">
+  </form>
+</body>
+</html>
